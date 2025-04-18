887cc934e65a59f9bd2cd80ddddc4c350f1b1bfe
FkihMehdi <mehdi.fkih@insat.ucar.tn>
2025-04-18 16:23:45 +0100

    first commit

---
+++ src/main/java/com/mehdifkih/sprint_boot_tp_devops/SprintBootTpDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.mehdifkih.sprint_boot_tp_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootTpDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootTpDevopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mehdifkih/sprint_boot_tp_devops/SprintBootTpDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehdifkih.sprint_boot_tp_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootTpDevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
